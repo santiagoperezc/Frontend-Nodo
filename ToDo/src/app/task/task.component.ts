97a485511d17478494bbb444f006b5a7efc4ad84
santiagoperezc <92562379+santiagoperezc@users.noreply.github.com>
2023-02-14 00:13:56 -0500

    Add files via upload

---
+++ ToDo/src/app/task/task.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent {
+  @Input() texto: string = ``;
+
+}
